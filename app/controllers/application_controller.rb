9461f943ef3bb30ab1f1bcbbd271ca517809794d
Sumit Birla <sbirla@tampahost.net>
2015-04-25 22:47:23 -0400

    added mobile format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :prepare_for_mobile
   helper_method :current_user
   helper_method :sysdate
   helper_method :systime
+  helper_method :mobile_device?
 
   private
-  
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this page"
-      redirect_to login_path
-    end
-  end
-  
-  def logged_in?
-    !!current_user
-  end
   
   def current_user
     @current_user ||= Cache.user(session[:user_id])
@@ -33,6 +24,18 @@ class ApplicationController < ActionController::Base
   def sysdate(time)
     time.in_time_zone(sys_time_zone).strftime("%m/%d/%Y").html_safe
   end
-
   
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+
 end
