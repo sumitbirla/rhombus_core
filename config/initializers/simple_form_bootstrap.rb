c88b354e5649ac1a202a0ecd536e402aa0c87a17
Sumit Birla <sbirla@tampahost.net>
2015-07-12 01:36:10 -0400

    simple_form update

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,7 +1,7 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.error_notification_class = 'alert alert-danger'
-  config.button_class = 'btn btn-default'
+  config.button_class = 'btn btn-primary'
   config.boolean_label_class = nil
 
   config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
@@ -125,5 +125,5 @@ SimpleForm.setup do |config|
   # Check the Bootstrap docs (http://getbootstrap.com)
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
-  config.default_wrapper = :vertical_form
+  config.default_wrapper = :horizontal_form
 end
