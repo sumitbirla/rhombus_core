8aaa24060f0cfea5791b88973a27fa8bb9feb7d5
Sumit Birla <sbirla@tampahost.net>
2017-01-09 07:28:29 -0500

    logged_in? helper added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   #before_filter :prepare_for_mobile
+  helper_method :logged_in?
   helper_method :current_user
   helper_method :sysdate
   helper_method :systime
   helper_method :mobile_device?
 
   private
+  
+  def logged_in?
+    session[:user_id]
+  end
   
   def current_user
     @current_user ||= Cache.user(session[:user_id])
