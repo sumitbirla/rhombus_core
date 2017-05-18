8cd807865f0e860862bcee783f681c2cd6252749
Sumit Birla <sbirla@tampahost.net>
2017-05-18 23:35:46 +0200

    mailer update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,14 +12,9 @@ class UserMailer < ActionMailer::Base
     from_name = Cache.setting(@user.domain_id, :system, 'From Email Name')
     from_email = Cache.setting(@user.domain_id, :system, 'From Email Address')
 
-    options = {
-        address: Cache.setting(@order.domain_id, :system, 'SMTP Server'),
-        openssl_verify_mode: 'none'
-    }
     mail(from: "#{from_name} <#{from_email}>",
          to: @user.email, 
-         subject: "Welcome to #{@website_name}", 
-         delivery_method_options: options)
+         subject: "Welcome to #{@website_name}")
   end
 
 
@@ -29,14 +24,9 @@ class UserMailer < ActionMailer::Base
     from_name = Cache.setting(user.domain_id, :system, 'From Email Name')
     from_email = Cache.setting(user.domain_id, :system, 'From Email Address')
 
-    options = {
-        address: Cache.setting(user.domain_id, :system, 'SMTP Server'),
-        openssl_verify_mode: 'none'
-    }
     mail( from: "#{from_name} <#{from_email}>",
           to: @user.email, 
-          subject: 'Password reset instructions', 
-          delivery_method_options: options)
+          subject: 'Password reset instructions')
   end
 
 end
