6c3516d12974aeccff5efc2777d2187b3d2b00c5
Sumit Birla <sbirla@tampahost.net>
2018-07-02 22:45:40 -0400

    update simple_form

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,5 +1,5 @@
 class DatepickerInput < SimpleForm::Inputs::Base
-  def input
+  def input(wrapper_options)
     @builder.text_field(attribute_name, { class: "form-control date-field" })
   end
 end
--- app/inputs/datetimepicker_input.rb
+++ app/inputs/datetimepicker_input.rb
@@ -1,5 +1,5 @@
 class DatetimepickerInput < SimpleForm::Inputs::Base
-  def input
+  def input(wrapper_options)
     value = object.send(attribute_name)
     value = value.strftime("%Y-%m-%d %I:%m %p") unless value.nil?
     @builder.text_field(attribute_name, { class: "form-control datetime-field", value: value })
