6dee0687b2b7777df0966c89dc259eee131f4b4e
Sumit Birla <sbirla@tampahost.net>
2019-09-03 16:55:31 +0200

    added per_page to base_controller

--- app/controllers/account/base_controller.rb
+++ app/controllers/account/base_controller.rb
@@ -2,6 +2,7 @@ class Account::BaseController < ApplicationController
   force_ssl  if Rails.env.production?
   
   before_action :require_login
+  before_action :set_page_size
   helper_method :current_user
 
 	layout 'account'
@@ -19,6 +20,15 @@ class Account::BaseController < ApplicationController
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def set_page_size
+    unless params[:per_page].blank?
+      cookies[:per_page] = params[:per_page]
+      @per_page = params[:per_page]
+    else
+      @per_page = cookies[:per_page] || 20
+    end
   end
 
 end
