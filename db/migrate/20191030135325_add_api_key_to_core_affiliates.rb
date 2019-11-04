6995cfb2bfc90e97c78b0d78300686ef3c27ab33
Sumit Birla <sbirla@tampahost.net>
2019-11-04 21:52:30 +0100

    fix migration class name API

--- db/migrate/20191030135325_add_api_key_to_core_affiliates.rb
+++ db/migrate/20191030135325_add_api_key_to_core_affiliates.rb
@@ -1,4 +1,4 @@
-class AddAPIKeyToCoreAffiliates < ActiveRecord::Migration[5.2]
+class AddApiKeyToCoreAffiliates < ActiveRecord::Migration[5.2]
   def change
     add_column :core_affiliates, :api_key, :string, after: :slug
   end
