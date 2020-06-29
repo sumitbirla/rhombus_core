fdbbf48c36c3beb110fb81701008aafd69068f11
Sumit Birla <sbirla@tampahost.net>
2020-06-29 09:42:16 -0400

    notifications DB update

--- db/migrate/20200625170929_create_events.rb
+++ db/migrate/20200625170929_create_events.rb
@@ -1,8 +1,8 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :core_events do |t|
-      t.integer :event_type_id
-      t.datetime :expires
+      t.integer :event_type_id, null: false
+      t.datetime :expires, null: false
       t.text :metadata
 
       t.timestamps
